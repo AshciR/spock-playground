8a95d7b8cbcccf0ed900e29a56ea60232e9da1c3
Richard Walker <richard.walker.90@gmail.com>
2018-06-02 17:09:15 -0400

    add surefire plugin

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -5,4 +5,28 @@ class FirstSpecification extends Specification {
         expect:
         1 + 1 == 2
     }
+
+    def "two + two should equal four"(){
+        given:
+            int left = 2
+            int right = 2
+
+        when:
+        int result = left + right
+
+        then:
+            result == 4
+    }
+
+    def "should remove from list"(){
+        given:
+            def list = [1, 2, 3, 4]
+
+        when:
+            list.remove(0)
+
+        then:
+            list == [2, 3, 4]
+    }
+
 }
