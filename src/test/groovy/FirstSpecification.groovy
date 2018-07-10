ed4a63243d3710e254e682e1382801feee68763b
Richard Walker <richard.walker.90@gmail.com>
2018-07-10 17:45:33 -0400

    add exception test example

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -1,6 +1,7 @@
 import spock.lang.Specification
 
 class FirstSpecification extends Specification {
+
     def "one plus one should equal two"() {
         expect:
         1 + 1 == 2
@@ -27,6 +28,18 @@ class FirstSpecification extends Specification {
 
         then:
             list == [2, 3, 4]
+    }
+
+    def "should throw a Index out of bounds exception"(){
+        given:
+            def list = [1, 2, 3, 4]
+
+        when:
+            list.remove(5)
+
+        then:
+            thrown(IndexOutOfBoundsException)
+            list.size() == 4
     }
 
 }
