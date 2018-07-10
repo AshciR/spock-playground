dcfc826697c63264a01a1b55678cd7b13528b18e
Richard Walker <richard.walker.90@gmail.com>
2018-07-10 19:20:34 -0400

    Add mock example for any value

--- src/test/groovy/MockingSpec.groovy
+++ src/test/groovy/MockingSpec.groovy
@@ -15,4 +15,31 @@ class MockingSpec extends Specification {
             statusCode == 0
     }
 
+    def 'Should return 201 when make a contribution of $100'(){
+        given:
+        def contributionMock = Mock(ContributionService)
+        contributionMock.makeContribution(100.00) >> 201
+
+        when:
+        def statusCode = contributionMock.makeContribution(100.00)
+
+        then:
+        statusCode == 201
+    }
+
+    def 'Should return 201 when we make any contribution'(double amount, int statusCode){
+        given:
+        def contributionMock = Mock(ContributionService)
+        contributionMock.makeContribution(_) >> 201
+
+        expect:
+        contributionMock.makeContribution(amount) == statusCode
+
+        where:
+        amount | statusCode
+        100.00 | 201
+        200.00 | 201
+        300.00 | 201
+
+    }
 }
